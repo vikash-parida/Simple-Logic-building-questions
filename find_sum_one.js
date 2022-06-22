0ed820f53471a45848b4f9b986bc7ce16f3c806a
vikash067 <vikash@nimapinfotech.com>
2022-06-22 10:13:40 +0530

    add else statement

--- find_sum_one.js
+++ find_sum_one.js
@@ -1,14 +1,16 @@
-function sumOfOne(arr){
+function sumOfZero(arr){
   
     for(let number of arr) {
      for(let j = 0; j<arr.length; j++) {
        if(number + arr[j] === 0 ){
         return [number, arr[j]]
+       }else{
+        return 'not a num in array this is sum of zero';
        }
         }
     }
 
 }
 
- let result = sumOfOne([-1,-2,-4,-5,0,9,5,8,2]);
+ let result = sumOfOne([-1,-4,-5,0,9,5,8,2]);
  console.log(result);
